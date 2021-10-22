d1d5216a1376a1c4e5b3309e31d2d5924cc01318
maoxiaoxing <930649054@qq.com>
2021-10-22 09:30:37 +0800

    push方法

--- JavaScript/学习JavaScript数据结构与算法（第3版）/第6章 链表/LinkedList.js
+++ JavaScript/学习JavaScript数据结构与算法（第3版）/第6章 链表/LinkedList.js
@@ -1,7 +1,16 @@
-import { defaultEquals } from './util'
-import { Node } from './linked-list-models'
 
-export default class LinkedList {
+
+function defaultEquals(a, b) {
+  return a === b
+}
+
+class Node {
+  constructor(element) {
+    this.element = element
+    this.next = undefined
+  }
+}
+class LinkedList {
   constructor(equalsFn = defaultEquals) {
     this.count = 0
     this.head = undefined
