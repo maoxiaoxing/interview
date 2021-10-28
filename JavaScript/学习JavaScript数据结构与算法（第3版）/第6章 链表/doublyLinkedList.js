7c6a49d27c4b1be7d9a7da5d42d6dd246eba81d5
maoxiaoxing <930649054@qq.com>
2021-10-28 16:34:25 +0800

    双向链表

--- JavaScript/学习JavaScript数据结构与算法（第3版）/第6章 链表/doublyLinkedList.js
+++ JavaScript/学习JavaScript数据结构与算法（第3版）/第6章 链表/doublyLinkedList.js
@@ -10,4 +10,37 @@ class DoublyLinkedList extends LinkedList {
     super(equalsFn)
     this.tail = undefined
   }
+
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new DoublyNode(element)
+      let current = this.head
+      if (index === 0) {
+        if (!this.head) {
+          this.head = node
+          this.tail = node
+        } else {
+          node.next = this.head
+          current.prev = node
+          this.head = node          
+        }
+      } else if (index === this.count) {
+        current = this.tail
+        console.log(current)
+        current.next = node
+        node.prev = current
+        this.tail = node
+      } else {
+        const prev = this.getElementAt(index - 1)
+        current = prev.next
+        prev.next = node
+        node.prev = prev
+        current.prev = node
+        node.next = current
+      }
+      this.count++
+      return true
+    }
+    return false   
+  }
 }
