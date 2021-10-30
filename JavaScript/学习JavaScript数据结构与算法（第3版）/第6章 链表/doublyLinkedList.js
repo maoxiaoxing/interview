a0ee8f929adef129639db3aa4dc5a3ab703ecfd2
maoxiaoxing <930649054@qq.com>
2021-10-30 09:27:40 +0800

    removeAt

--- JavaScript/学习JavaScript数据结构与算法（第3版）/第6章 链表/doublyLinkedList.js
+++ JavaScript/学习JavaScript数据结构与算法（第3版）/第6章 链表/doublyLinkedList.js
@@ -45,6 +45,27 @@ class DoublyLinkedList extends LinkedList {
   }
 
   removeAt(index) {
-    
+    if (index >= 0 && index < this.count) {
+      let current = this.head
+      if (index === 0) {
+        if (this.size()) {
+          this.head = undefined
+        } else {
+          const removed = this.head
+          current = this.getElementAt(this.size())
+          this.head = this.head.next
+          current.next = this.head
+          current = removed
+        }
+      } else {
+        const prev = this.getElementAt(index-1)
+        current = prev.next
+        prev.next = current.next
+      }
+
+      this.count--
+      return current.element
+    }
+    return undefined
   }
 }
