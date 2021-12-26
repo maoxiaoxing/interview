ce2c385d85acf15c7e9c3ede42e9acc19ca1197a
maoxiaoxing <930649054@qq.com>
2021-12-26 20:26:29 +0800

    describe

--- autotest/jest01/global-api.test.js
+++ autotest/jest01/global-api.test.js
@@ -11,10 +11,21 @@
 // })
 
 
-test('global-api test', () => {
-  expect(2+2).toBe(4)
-  expect({name: 'jack'}).toEqual({name: 'jack'})
-  expect('barfoo').toMatch(/foo/)
-  expect(6).toBeGreaterThan(2)
-  expect(6).toBeLessThan(8)
-})
+// test('global-api test', () => {
+//   expect(2+2).toBe(4)
+//   expect({name: 'jack'}).toEqual({name: 'jack'})
+//   expect('barfoo').toMatch(/foo/)
+//   expect(6).toBeGreaterThan(2)
+//   expect(6).toBeLessThan(8)
+// })
+
+
+// describe('a组件', () => {
+//   test('a', () => {
+//     console.log('a 组件的 a 功能')
+//   })
+
+//   test('b', () => {
+//     console.log('a 组件的 b 功能')
+//   })
+// })
