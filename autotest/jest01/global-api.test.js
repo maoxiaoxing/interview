0dc14579ffee9e3b41ec4310df4c3fca1ef308d8
maoxiaoxing <930649054@qq.com>
2021-12-26 20:23:04 +0800

    断言函数

--- autotest/jest01/global-api.test.js
+++ autotest/jest01/global-api.test.js
@@ -1,12 +1,20 @@
-test('global-api test', () => {
-  console.log('global api')
-})
+// test('global-api test', () => {
+//   console.log('global api')
+// })
 
-test.only('global-api test', () => {
-  console.log('test only')
-})
+// test.only('global-api test', () => {
+//   console.log('test only')
+// })
 
 // test.only('global-api test', () => {
 //   console.log('test only1')
 // })
 
+
+test('global-api test', () => {
+  expect(2+2).toBe(4)
+  expect({name: 'jack'}).toEqual({name: 'jack'})
+  expect('barfoo').toMatch(/foo/)
+  expect(6).toBeGreaterThan(2)
+  expect(6).toBeLessThan(8)
+})
