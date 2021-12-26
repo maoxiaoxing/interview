3579805bbd50e57c9196ee7654199f129baaf6ca
maoxiaoxing <930649054@qq.com>
2021-12-26 20:17:28 +0800

    test only

---
+++ autotest/jest01/global-api.test.js
@@ -0,0 +1,12 @@
+test('global-api test', () => {
+  console.log('global api')
+})
+
+test.only('global-api test', () => {
+  console.log('test only')
+})
+
+// test.only('global-api test', () => {
+//   console.log('test only1')
+// })
+
