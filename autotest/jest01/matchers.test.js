a97e0b4d6b93dc00840c23006820fbd5f1f5a581
maoxiaoxing <930649054@qq.com>
2021-12-27 08:18:38 +0800

    常用匹配器

--- autotest/jest01/matchers.test.js
+++ autotest/jest01/matchers.test.js
@@ -19,3 +19,29 @@ test('zero', () => {
   expect(z).not.toBeTruthy()
   expect(z).toBeFalsy()
 })
+
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe and toEqual are equivalent for numbers
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
+
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3);           This won't work because of rounding error
+  expect(value).toBeCloseTo(0.3); // This works.
+});
+
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/);
+});
