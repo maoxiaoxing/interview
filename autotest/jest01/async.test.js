9681f5c028b3c9ba8e46f114334d1519e7a6f4ca
maoxiaoxing <930649054@qq.com>
2021-12-27 08:29:04 +0800

    测试异步代码

---
+++ autotest/jest01/async.test.js
@@ -0,0 +1,39 @@
+
+// const getData = (cb) => {
+//   setTimeout(() => {
+//     cb({foo: 'bar'})
+//   }, 2000)
+// }
+
+// test('async test', (done) => {
+//   getData((data) => {
+//     done()
+//     expect(data).toEqual({foo: 'bar'})
+//   })
+// })
+
+const getData = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // cb({foo: 'bar'})
+      resolve({foo: 'bar'})
+    }, 2000)
+  })
+}
+
+// test('async test', (done) => {
+//   getData().then((data) => {
+//     done()
+//     expect(data).toEqual({foo: 'bar'})
+//   })
+// })
+
+// test('async test', () => {
+//   return getData().then((data) => {
+//     expect(data).toEqual({foo: 'bar'})
+//   })
+// })
+
+test('async test', () => {
+  return expect(getData()).resolves.toEqual({foo: 'bar'})
+})
