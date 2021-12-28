6c727fe22e69d8b2b45a0755dce5f0f67949bef7
maoxiaoxing <930649054@qq.com>
2021-12-28 08:35:23 +0800

    mock 函数

---
+++ autotest/jest01/mock-function.test.js
@@ -0,0 +1,22 @@
+
+function forEach(items, cb) {
+  for (let index = 0; index < items.length; index++) {
+    cb(items[index], index)
+  }
+}
+
+test('mock func', () => {
+  const testArr = [1,2,3]
+  const mockFn = jest.fn((value, index) => {
+    return value + index
+  })
+
+  // mock 所有函数返回值
+  // mockFn.mockReturnValue(123)
+
+  // mock 一次返回值
+  mockFn.mockReturnValueOnce(123)
+  forEach(testArr, mockFn)
+  console.log(mockFn.mock)
+  expect(mockFn.mock.calls.length).toBe(testArr.length)
+})
