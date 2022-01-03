8f32e612d5d9d08d6c4c7b9e8a380d51f9800fca
maoxiaoxing <930649054@qq.com>
2022-01-03 20:36:17 +0800

    class

--- typeScript/ts_in_action/src/class.ts
+++ typeScript/ts_in_action/src/class.ts
@@ -25,3 +25,57 @@ class Husky extends Dog {
   private legs: number = 4
 }
 
+abstract class Animal {
+  constructor(name: string) {
+      this.name = name
+  }
+  protected name: string
+  eat () {}
+  abstract sleep (): void
+}
+
+class Panda extends Animal {
+  constructor(name: string) {
+    super(name)
+  }
+  sleep () {
+    console.log(`${this.name} sleep`)
+  }
+}
+
+class Cat extends Animal {
+  constructor(name: string) {
+    super(name)
+  }
+
+  sleep () {
+    console.log(`${this.name} sleep`)
+  }
+}
+const cat = new Cat('maomao')
+
+const panda = new Panda('panpan')
+
+const animals: Animal[] = [panda, cat]
+animals.forEach((animal) => {
+  animal.sleep()
+})
+
+class WorkFlow {
+  stet1() {
+    return this
+  }
+  stet2() {
+    return this
+  }
+}
+
+new WorkFlow().stet1().stet2()
+
+class Myflow extends WorkFlow {
+  next() {
+    return this
+  }
+}
+
+new Myflow().next().stet1().next().stet2()
