57f78603916145b61c3d279ef92fc015bdf9685d
maoxiaoxing <930649054@qq.com>
2022-01-04 07:39:45 +0800

    类型约束

--- typeScript/ts_in_action/src/generics.ts
+++ typeScript/ts_in_action/src/generics.ts
@@ -12,3 +12,25 @@ interface Log<T = string> {
 
 const myLog: Log<number> = log
 
+
+class Log1<T> {
+  run(value: T) {
+    return value
+  }
+}
+
+const log1 = new Log1<number>()
+log1.run(1)
+
+
+// 类型约束
+
+interface Length {
+  length: number
+}
+
+function log2<T extends Length>(value:T) {
+  console.log(value.length)
+  return value
+}
+
