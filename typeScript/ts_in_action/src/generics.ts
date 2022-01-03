73db544e3eeacaa7917403d7bf41543e6472a61f
maoxiaoxing <930649054@qq.com>
2022-01-03 22:01:47 +0800

    泛型

---
+++ typeScript/ts_in_action/src/generics.ts
@@ -0,0 +1,13 @@
+
+function log<T>(value: T): T {
+  return value
+}
+
+// type Log = <T>(value: T) => T
+// const myLog: Log = log
+
+interface Log<T = string> {
+  <T>(value: T): T;
+}
+
+const myLog: Log<number> = log
