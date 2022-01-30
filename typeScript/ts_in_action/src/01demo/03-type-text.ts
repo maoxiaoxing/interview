3709edf028e6a504c936ff9316f61a3f1c927781
maoxiaoxing <930649054@qq.com>
2022-01-30 22:13:44 +0800

    只读属性

--- typeScript/ts_in_action/src/01demo/03-type-text.ts
+++ typeScript/ts_in_action/src/01demo/03-type-text.ts
@@ -1,3 +1,16 @@
 export type Direction = 'right' | 'left'
 
 const direction: Direction = 'right'
+
+function handleRequest (url: string, method: 'GET' | 'POST' | 'OPTION') {
+
+}
+
+// 对象所有属性变成只读属性
+const req = {
+  url: 'https://example.com',
+  method: 'GET',
+  obj: {a: 1},
+} as const
+
+handleRequest(req.url, req.method)
