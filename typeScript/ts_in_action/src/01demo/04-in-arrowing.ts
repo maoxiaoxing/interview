5c1f665ebf068b5118414bd4f1534764bf87f56c
maoxiaoxing <930649054@qq.com>
2022-01-31 21:58:55 +0800

    in 缩小

---
+++ typeScript/ts_in_action/src/01demo/04-in-arrowing.ts
@@ -0,0 +1,14 @@
+export type Fish = {
+  swim: () => void
+}
+
+type Dog = {
+  run: () => void
+}
+
+function move (aniaml: Fish | Dog) {
+  if ('swim' in aniaml) {
+    return aniaml.swim()
+  }
+  return aniaml.run()
+}
