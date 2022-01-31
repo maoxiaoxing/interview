ca5cbd162c62e2e479c9c3e2fcf124f78a5c57a0
maoxiaoxing <930649054@qq.com>
2022-01-31 22:14:01 +0800

    控制流分析

---
+++ typeScript/ts_in_action/src/01demo/04-control-flow.ts
@@ -0,0 +1,17 @@
+// 控制流分析
+export function example () {
+  let x: number | string | boolean
+
+  x = Math.random() < .5
+  if (Math.random() < 0.5) {
+    x = 1
+  } else {
+    x = '1'
+  }
+  return x
+}
+
+let x = example()
+x = 1
+x = '1'
+// x = true
