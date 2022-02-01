7c2ceaf52b79fbfcf625f8f01c653252508b6dd9
maoxiaoxing <930649054@qq.com>
2022-02-01 10:37:22 +0800

    构造签名

---
+++ typeScript/ts_in_action/src/01demo/05func/05-constructor-sign.ts
@@ -0,0 +1,37 @@
+// 构造签名
+export class Ctor {
+  s: string;
+  constructor(s: string) {
+    this.s = s
+  }
+}
+
+type MxxContructor = {
+  new (s: string): Ctor
+}
+
+function mxxHandleCtor(C: MxxContructor) {
+  const c = new C('mxx')
+  return c
+}
+
+const mxx = mxxHandleCtor(Ctor)
+console.log(mxx.s)
+
+interface MxxDateConstructor {
+  new (s: string): Date;
+  (s: number): number
+}
+
+function MxxDate (s: string | number) {
+  if (typeof s === 'number') return s
+  return new Date(s)
+}
+
+function handleMxxDate(MxxDate: MxxDateConstructor) {
+  const d = new MxxDate('2022-02-01')
+  const d1 = MxxDate(100)
+  console.log(d)
+  console.log(d1)
+}
+handleMxxDate(MxxDate as MxxDateConstructor)
--- typeScript/ts_in_action/src/index.ts
+++ typeScript/ts_in_action/src/index.ts
@@ -7,7 +7,8 @@
 // import './01demo/04narrowing/04-equality-arrowing'
 
 // import './01demo/05func/05-expression'
-import './01demo/05func/05-sign'
+// import './01demo/05func/05-sign'
+import './01demo/05func/05-constructor-sign'
 
 const hello: string = 'Hello TypeScript'
 
