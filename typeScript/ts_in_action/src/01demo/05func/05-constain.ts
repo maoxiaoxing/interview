551888d1cdf71d07a6262bab18d1ece917420246
maoxiaoxing <930649054@qq.com>
2022-02-01 20:54:01 +0800

    限制条件

---
+++ typeScript/ts_in_action/src/01demo/05func/05-constain.ts
@@ -0,0 +1,11 @@
+interface Length {
+  length: number;
+}
+
+export function longest<T extends Length> (x: T, y: T): T {
+  if (x.length > y.length) return x
+  return y
+}
+
+longest([1], [2,3])
+longest('123', '4567')
