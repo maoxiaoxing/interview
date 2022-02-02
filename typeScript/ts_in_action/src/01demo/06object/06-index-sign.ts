a0f369104264b81de2cb8b9aac77e08d9eaba472
maoxiaoxing <930649054@qq.com>
2022-02-02 14:42:21 +0800

    索引签名

---
+++ typeScript/ts_in_action/src/01demo/06object/06-index-sign.ts
@@ -0,0 +1,14 @@
+export interface StringArr {
+  [index: number]: string
+}
+
+const arr: StringArr = ['1']
+
+interface StrObj {
+  [props: string]: number | string
+}
+
+const obj: StrObj = {
+  1: '1',
+  '2': '2'
+}
