728c6bc9ebf59cf1cf92b1f0e89c46dcf1ac7421
maoxiaoxing <930649054@qq.com>
2022-02-06 17:33:51 +0800

    原地快排

--- typeScript/ts_in_action/src/01demo/07-generic/07-infer.ts
+++ typeScript/ts_in_action/src/01demo/07-generic/07-infer.ts
@@ -4,6 +4,7 @@ export type GetReturnType<T> = T extends (arg: any) => infer R ? R : any
 type Num = GetReturnType<() => number>
 
 type Time = GetReturnType<typeof setTimeout>
+type Time1 = ReturnType<typeof setTimeout>
 
 type Flatten<T> = T extends Array<infer U> ? U : any
 type T1 = [number, string]
