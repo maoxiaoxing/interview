cbc7db02713c6d634f1cf743d79efdfdd7fb4170
maoxiaoxing <930649054@qq.com>
2022-02-12 21:47:20 +0800

    marketing

---
+++ micro-front/applications/marketing/webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+const packageJSON = require("./package.json")
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    // new ModuleFederationPlugin({
+    //   name: "marketing",
+    //   filename: "remoteEntry.js",
+    //   exposes: {
+    //     "./MarketingApp": "./src/bootstrap.js"
+    //   },
+    //   shared: packageJSON.dependencies
+    // }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+}
