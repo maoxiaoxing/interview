65c7ddc24199a9bd99d0037f646c2e0de6974f75
maoxiaoxing <930649054@qq.com>
2022-02-12 21:59:01 +0800

    安装react

--- micro-front/applications/marketing/src/bootstrap.js
+++ micro-front/applications/marketing/src/bootstrap.js
@@ -1,1 +1,15 @@
-console.log('marketing')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function mount (el) {
+  ReactDOM.render(<div>Marketing</div>, el)
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#app-marketing')
+  if (el) mount(el)
+}
+
+export {
+  mount,
+}
