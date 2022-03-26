eb47081af0933ea2fa82f518ddad009aec23e663
maoxiaoxing <930649054@qq.com>
2022-03-26 17:17:50 +0800

    Node 和 insertNode

---
+++ algorithm/tree/binary-search-tree.js
@@ -0,0 +1,36 @@
+export class Node {
+  left
+  right
+  key
+  constructor(key) {
+    this.left = null
+    this.right = null
+    this.key = key
+  }
+}
+
+export default class BinarySearchTree {
+  root
+  constructor() {
+    this.root = null
+  }
+
+  insertNode(node, key) {
+    if (!node) return null
+    if (key > node.key) {
+      // 目标 key 大于当前节点的 key 且 当前节点的右侧没有节点，则将 key 插入当前节点 右侧
+      if (!node.right) {
+        node.right = new Node(key)
+      } else {
+        // 否则递归插入
+        this.insertNode(node.right, key)
+      }
+    } else {
+      if (!node.left) {
+        node.left = new Node(key)
+      } else {
+        this.insertNode(node.left, key)
+      }
+    }
+  }
+}
