aaea6a7385a8085b7f43cf068ba33f8b7bf1c8cf
maoxiaoxing <930649054@qq.com>
2022-05-07 08:28:59 +0800

    缓存

--- JavaScript/Service Worker/example/sw.js
+++ JavaScript/Service Worker/example/sw.js
@@ -1,4 +1,29 @@
+let VERSION = 'v1'
 
-self.addEventListener('install', function (e) {
-  
+// 缓存
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(VERSION)
+      .then(function (cache) {
+        return cache.addAll([
+          './test.html'
+        ])
+      })
+  )
 })
+
+// 缓存更新
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          // 如果当前版本和缓存版本不一致
+          if (cacheName !== VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
