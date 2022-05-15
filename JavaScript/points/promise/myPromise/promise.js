984108fbec7d3abf3b283599fecba3b1d836b77a
maoxiaoxing <930649054@qq.com>
2022-05-15 09:58:52 +0800

    init status

--- JavaScript/points/promise/myPromise/promise.js
+++ JavaScript/points/promise/myPromise/promise.js
@@ -1,5 +1,25 @@
-
+const REJECTED = 'rejected'
+const FUFILLED = 'fufilled'
+const PENDING = 'pending'
 
 class MyPromise {
+  constructor (executor) {
+    try {
+      executor(this.resolve, this.reject)
+    } catch (err) {
+      this.reject(err)
+    }
+  }
 
+  status = PENDING
+  value = undefined
+  reason = undefined
+  
+  resolve () {
+
+  }
+
+  reject () {
+
+  }
 }
