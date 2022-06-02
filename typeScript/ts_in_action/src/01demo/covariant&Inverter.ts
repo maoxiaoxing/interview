576a65dd1761079050733f25d47480666f3c95a9
maoxiaoxing <930649054@qq.com>
2022-06-02 09:39:58 +0800

    逆变和协变

--- typeScript/ts_in_action/src/01demo/covariant&Inverter.ts
+++ typeScript/ts_in_action/src/01demo/covariant&Inverter.ts
@@ -1,34 +1,36 @@
 
 class Animal {
-  doAnimalThing(): void {}
+  name: string
 }
 
-class Dog extends Animal {
-  doDongThing(): void {}
+class Bird extends Animal {
+  fly() {}
 }
 
-class Cat extends Animal {
-  doCatThing(): void {}
+/*
+  协变
+  A 是 B 父类 即 B extends A
+  B 赋值给 A 是协变
+*/
+let animal: Animal = new Animal()
+animal = new Bird() // 协变
+
+let fun1 = () => Animal
+let fun2 = () => Bird
+fun1 = fun2
+// fun2 = fun1 // 父类型不能赋值给子类型
+
+
+let fn1: (x: Animal) => void = (x: Animal) => {
+  console.log(x.name)
 }
 
-function makeAnimalAciton(animalAction: (animal: Animal) => void):void {
-  let cat: Cat = new Cat()
-  animalAction(cat)
+let fn2: (x: Bird) => void = (x: Bird) => {
+  x.fly()
 }
 
-type AnimalAction = (animal: Animal) => void
-type DogAction = (dog: Dog) => void
-
-let dogAction: DogAction = (dog: Dog) => {
-  dog.doDongThing()
-}
-
-let animalAction: AnimalAction = dogAction
-
-makeAnimalAciton(animalAction)
-
-
-
+fn1 = fn2
+fn2 = fn1
 
 
 
