48a03b725bae4377c4d343ca518d87773742913f
maoxiaoxing <930649054@qq.com>
2022-07-03 23:05:12 +0800

    nextTick

---
+++ JavaScript/points/nextTick/index.js
@@ -0,0 +1,46 @@
+let pending = false
+const callbacks = []
+
+let timerFunc
+
+function flushCallbacks () {
+  pending = false
+  const copies = callbacks.slice(0)
+  callbacks.length = 0
+  for (let i = 0; i < copies.length; i++) {
+    copies[i]()
+  }
+}
+
+timerFunc = () => {
+  setTimeout(flushCallbacks, 0)
+}
+
+function nextTick (cb, ctx) {
+  let _resolve
+  // 把 cb 加上异常处理存入 callbacks 数组中
+  callbacks.push(() => {
+    if (cb) {
+      try {
+        // 调用 cb()
+        cb.call(ctx)
+      } catch (e) {
+        handleError(e, ctx, 'nextTick')
+      }
+    } else if (_resolve) {
+      _resolve(ctx)
+    }
+  })
+  if (!pending) {
+    pending = true
+    // 调用
+    timerFunc()
+  }
+  // $flow-disable-line
+  if (!cb && typeof Promise !== 'undefined') {
+    // 返回 promise 对象
+    return new Promise(resolve => {
+      _resolve = resolve
+    })
+  }
+}
